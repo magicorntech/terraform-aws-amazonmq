6edce63ca74a1022f6196dd1639334d0219b673f
Umur Coskuncan | Magicorn <umur@magicorn.co>
2023-06-22 18:34:11 +0300

    Remove sg ignore

--- sg.tf
+++ sg.tf
@@ -24,10 +24,6 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  lifecycle {
-    ignore_changes = [ingress]
-  }
-
   tags = {
     Name        = "${var.tenant}-${var.name}-amazonmq-${var.mq_name}-sg-${var.environment}"
     Tenant      = var.tenant
